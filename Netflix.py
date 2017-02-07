05f7cd486b8b21271690b920c0821e266b18c9cf
Ellen Wyllie <ellen@cs.utexas.edu>
2017-02-07 16:52:53 -0600

    broken code

--- Netflix.py
+++ Netflix.py
@@ -10,7 +10,7 @@ from requests import get
 from os import path
 from numpy import sqrt, square, mean, subtract
 
-'''
+
 def create_cache(filename):
     """
     filename is the name of the cache file to load
@@ -39,11 +39,41 @@ AVERAGE_MOVIE_RATING_PER_YEAR = create_cache(
 YEAR_OF_RATING = create_cache("cache-yearCustomerRatedMovie.pickle")
 CUSTOMER_AVERAGE_RATING_YEARLY = create_cache(
     "cache-customerAverageRatingByYear.pickle")
-'''
 
-actual_scores_cache ={10040: {2417853: 1, 1207062: 2, 2487973: 3}}
-movie_year_cache = {10040: 1990}
-decade_avg_cache = {1990: 2.4}
+
+
+#actual_scores_cache = {10040: {2417853: 1, 1207062: 2, 2487973: 3}}
+actual_scores_cache = {}
+cust_id_rating = {}
+for item in ACTUAL_CUSTOMER_RATING.items():
+    customer_id = item[0][0]
+    movie_id = item[0][1]
+    rating = item[1]
+    cust_id_rating[customer_id] = rating 
+    actual_scores_cache[movie_id] = cust_id_rating
+
+#movie_year_cache = {10040: 1990}
+movie_year_cache = {}
+for item in AVERAGE_MOVIE_RATING_PER_YEAR.items():
+    movie_id = item[0][0]
+    year = item[0][1]
+    rating = item[1]
+    movie_year_cache[movie_id] = year
+print(movie_year_cache)
+
+#decade_avg_cache = {1990: 2.5, 2000: 2.6}
+#decade_avg_cache = {}
+decade_ratings = {199: [], 200: []}
+for movie in actual_scores_cache:
+    for film in movie_year_cache:
+        if movie == film:
+            year = (movie_year_cache[film] // 10)
+            if year in decade_ratings:
+                decade_ratings[year].append(actual_scores_cache[movie])
+
+print(decade_ratings)
+
+
 
 # ------------
 # netflix_eval
